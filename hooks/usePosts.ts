bad2876463b6259b8a33fe1c7e7d02253474166d
cyrilletual <cyrille.tual@gmail.com>
2024-01-11 17:12:24 +0100

    before db modifications

--- hooks/usePosts.ts
+++ hooks/usePosts.ts
@@ -2,14 +2,22 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import { Post } from "@prisma/client";
 
-const getAllPosts = async () => {
-  const { data } = await axios.get(`../api/posts`);
-  return data as Post;
-};
+// const getAllPosts = async () => {
+//   const { data } = await axios.get(`../api/posts`);
+//   return data as Post;
+// };
+// export function usePosts() {
+//   return useQuery({
+//     queryKey: ["posts"],
+//     queryFn: () => getAllPosts(),
+//   });
+// }
+
+// même chose mais plus concentré :
 
 export function usePosts() {
-  return useQuery({
-    queryKey: ["posts"],
-    queryFn: () => getAllPosts(),
+  return useQuery("posts", async () => {
+    const { data } = await axios.get(`../api/posts`);
+    return data as Post;
   });
 }
