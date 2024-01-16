28c3dba8726ebb4df77f614b05b3c3fca4281971
cyrilletual <cyrille.tual@gmail.com>
2024-01-16 13:13:35 +0100

    fix cat error

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -3,8 +3,9 @@ import PageContainer from "@/components/page-container";
 import PostCard from "@/components/post-card";
 import PageTitle from "@/components/ui/page-title";
 import { usePostsByCategory } from "@/hooks/usePosts";
+import { PostWithCategory } from "@/types";
 
-import { Post } from "@prisma/client";
+
 
 type Params = {
   params: {
@@ -21,7 +22,7 @@ export default function Caterorypage({ params }: Params) {
   } = usePostsByCategory(slug);
 
   // Explicitly typing items as Post[]
-  const items: Post[] | undefined = data ;
+  const items: PostWithCategory[] | undefined = data ;
 
   return (
     <>
@@ -30,7 +31,7 @@ export default function Caterorypage({ params }: Params) {
           <PageTitle title={slug.charAt(0).toUpperCase() + slug.slice(1)} />
           {isSuccess && (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 mt-6">
-              {items.map((post: Post) => (
+              {items.map((post: PostWithCategory) => (
                 <PostCard key={post.id} post={post} />
               ))}
             </div>
